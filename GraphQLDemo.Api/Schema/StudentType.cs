aef0ffdc50fe04761aef694b40904fc3c0d6e989
Amir <mosallanejad.amir@gmail.com>
2025-05-28 15:12:29 +0330

    Add Schema types and modify query for getting list of types and GetById

---
+++ GraphQLDemo.Api/Schema/StudentType.cs
@@ -0,0 +1,11 @@
+﻿namespace GraphQLDemo.Api.Schema;
+
+public class StudentType
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    [GraphQLName("gpa")]
+    public double GPA { get; set; }
+}
